04ca723995bae7651e2206c683faf0e83577731e
fatefaker00 <43573546+fatefaker00@users.noreply.github.com>
2018-09-29 16:29:03 +0700

    Update Changelogs.lua

--- afreefire/Changelogs.lua
+++ afreefire/Changelogs.lua
@@ -1,9 +1,6 @@
-function changes()
-    
-    print('Change Logs: \n\n
+  print('Change Logs: \n\n
           Version _1.0_: \n 
           ● Avatar Contrast \n 
           ● 3D Flat Vehicle \n 
           ● Sky Contrast \n 
           ● Thermal Maps')
-end
