5f315f35fb3b4853a6df81b850111525e9d8c67f
fatefaker00 <43573546+fatefaker00@users.noreply.github.com>
2018-09-29 15:48:57 +0700

    Create Changelogs.lua

---
+++ afreefire/Changelogs.lua
@@ -0,0 +1,12 @@
+while (true) do
+	if gg.isVisible(true) then
+		gg.setVisible(false)
+    
+    gg.alert('Change Logs: \n\n
+          Version _1.0_: \n 
+          ● Avatar Contrast \n 
+          ● 3D Flat Vehicle \n 
+          ● Sky Contrast \n 
+          ● Thermal Maps')
+  end
+end
