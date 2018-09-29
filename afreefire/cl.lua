387fd2f0bf32d24e801d8c045020f46b0ad9d36b
fatefaker00 <43573546+fatefaker00@users.noreply.github.com>
2018-09-29 18:35:35 +0700

    Delete cl.lua

--- afreefire/cl.lua
+++
@@ -1,8 +0,0 @@
-gg.setVisible(ShowUI)
-gg.clearResults()
-gg.alert('Change Logs: \n\n
-          Version _1.0_: \n 
-          ● Avatar Contrast \n 
-          ● 3D Flat Vehicle \n 
-          ● Sky Contrast \n 
-          ● Thermal Maps')
